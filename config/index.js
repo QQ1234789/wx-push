aaa6b3eecde21d47b8187e366c09dc251287870e
QQ1234789 <34230958+QQ1234789@users.noreply.github.com>
2023-11-23 14:57:00 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 module.exports = {
-  appid: "wxa9189ded357b0ec6",
-  appsecret: "eaf0976f6bc9cf3c65f46443678215f4",
-  template_id: "yKX9psJ-5tfiEB1cOLwRqpcqyB3il5Lg0tT_roYN4FE",
-  touser: "o1Qvh6kbC8i97UUa7UbZYOFveqn0",
+  appid: "wxa9189ded357b0ec",
+  appsecret: "eaf0976f6bc9cf3c65f46443678215f",
+  template_id: "yKX9psJ-5tfiEB1cOLwRqpcqyB3il5Lg0tT_roYN4F",
+  touser: "o1Qvh6kbC8i97UUa7UbZYOFveqn",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "北京",
